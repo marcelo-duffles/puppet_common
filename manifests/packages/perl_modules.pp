99a41cef32a93ddb540529b5bff8dd5a6e660628
Marcelo D. D. Moreira <marceloddm@gmail.com>
2013-01-30 12:10:33 -0200

    perl modules

---
+++ manifests/packages/perl_modules.pp
@@ -0,0 +1,6 @@
+
+class puppet_common::packages::perl_modules {
+	
+  include puppet_common::packages::perl_time_hires
+  
+}
---
+++ manifests/packages/perl_time_hires.pp
@@ -0,0 +1,6 @@
+
+class puppet_common::packages::perl_time_hires {
+	
+  package{['perl-Time-HiRes']: }
+  
+}
