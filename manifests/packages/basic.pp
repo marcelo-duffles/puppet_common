f6f10f1b186557ec74e82f40ad0083a94bdbe7af
João Víctor Rocon Maia <maia.jvrm@gmail.com>
2013-08-24 07:47:21 -0300

    added tmux

--- manifests/packages/basic.pp
+++ manifests/packages/basic.pp
@@ -3,6 +3,13 @@ class puppet_common::packages::basic {
 	
   package{['lsof', 'mlocate']: }
   package{['vim-enhanced']: }
+  package{['tmux']: }
+  file { '.tmux.conf':
+    path    => '/root/.tmux.conf',
+    ensure  => file,
+    require => Package['tmux'],
+    source  => "puppet:///modules/puppet_common/tmux_conf"
+  }
   package{['telnet', 'mtr' ]: }
   include puppet_common::packages::augeas
   
