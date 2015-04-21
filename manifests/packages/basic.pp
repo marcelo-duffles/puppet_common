46ea43660de8af557d3287c9f131e02a8a7f7f47
Marcelo D. D. Moreira <marceloddm@gmail.com>
2015-04-21 19:00:19 -0300

    added bind-utils package

--- manifests/packages/basic.pp
+++ manifests/packages/basic.pp
@@ -3,7 +3,7 @@ class puppet_common::packages::basic {
   package{['lsof', 'mlocate', 'tree']: }
   package{['vim-enhanced']: }
   package{['screen']: }
-  package{['telnet', 'mtr' ]: }
+  package{['telnet', 'mtr', 'bind-utils' ]: }
   include puppet_common::packages::augeas
   
 }
