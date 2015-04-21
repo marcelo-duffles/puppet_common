484abf018620bdb6b7e6d160172c8117d6d91e6a
Marcelo D. D. Moreira <marceloddm@gmail.com>
2015-04-21 12:19:49 -0300

    tmux replaced for screen

--- manifests/packages/basic.pp
+++ manifests/packages/basic.pp
@@ -2,13 +2,7 @@ class puppet_common::packages::basic {
 	
   package{['lsof', 'mlocate', 'tree']: }
   package{['vim-enhanced']: }
-  package{['tmux']: }
-  file { '.tmux.conf':
-    path    => '/root/.tmux.conf',
-    ensure  => file,
-    require => Package['tmux'],
-    source  => "puppet:///modules/${module_name}/tmux_conf"
-  }
+  package{['screen']: }
   package{['telnet', 'mtr' ]: }
   include puppet_common::packages::augeas
   
