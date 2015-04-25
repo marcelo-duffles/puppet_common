77a78b49afb65a3fd3f64cd05cd69d684eb3e8e2
Marcelo D. D. Moreira <marceloddm@gmail.com>
2015-04-25 10:58:56 -0300

    reorganizing packages

--- manifests/packages/basic.pp
+++ manifests/packages/basic.pp
@@ -3,8 +3,10 @@ class puppet_common::packages::basic {
   package{['lsof', 'mlocate', 'tree']: }
   package{['vim-enhanced']: }
   package{['screen']: }
-  package{['iotop']: }
-  package{['telnet', 'mtr', 'bind-utils' ]: }
+  package{['iotop', 'sysstat']: }
+  package{['telnet', 'mtr', 'bind-utils', 'tcpdump' ]: }
+  package{['lrzsz', 'lrzip']: } # Used to download/upload files from server
+  
   include puppet_common::packages::augeas
   
 }
--- manifests/packages/network.pp
+++ manifests/packages/network.pp
@@ -2,10 +2,6 @@
 class puppet_common::packages::network {
 	
   package{['iptraf']: }
-  package{['lrzsz', 'lrzip']: } # Use to download/upload files from server
-  package{['tcpdump']: }
-  package{['sysstat']: }
-  package{['dstat']: }
   package{['nmap']: }
      
 }
